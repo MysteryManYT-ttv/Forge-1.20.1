530c89bc025799b82dd05b195b91069abf9e90a7
MysteryManYT-ttv <aidenrebec09@gmail.com>
2026-09-11 14:29:37 -0400

    Forge-install-script.sh
    version 1.3

--- Forge-server-setup.sh
+++ Forge-server-setup.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 #Madlib Copyright MysteryManYT-ttv 2026
-echo "Downloading Forge version 1.20.1"
+echo "Downloading Forge version 1.20.1 :) "
 
-#wget -P ~/Downloads https://github.com/MysteryManYT-ttv/Forge-1.20.1/archive/refs/heads/main.zip"
+
 wget "https://github.com/MysteryManYT-ttv/Forge-1.20.1/archive/refs/heads/main.zip"
+
+mkdir ~/Downloads/main
+chmod +x ~/main.zip
 unzip ~/main.zip
 mv ~/main.zip ~/Downloads/main
+mv ~/Forge-1.20.1-main ~/Documents
+mv ~/Documents/Forge-1.20.1-main/./forge-1.20.1-47.4.10-installer-1-.jar ~/Documents
 
-#mv ~/main/Forge-1.20.1-main/ ~/Downloads
-#chmod +x  ~/main/Forge-1.20.1-main/forge-1.20.1-47.4.10-installer(1).jar"
+rm -fr ~/Documents/Forge-1.20.1-main
+rm -fr ~/Downloads/main
+sudo apt install openjdk-21-jdk
 
-
-
-
-
-
-
-
-
+#chmod +x ~/Documents/./forge-1.20.1-47.4.10-instaler-1-.jar
 #work in progress :)
 
 
