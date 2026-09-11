f4cf6633fc7b634a68df6e2ef49557d0c5174c45
MysteryManYT-ttv <aidenrebec09@gmail.com>
2026-09-11 12:42:19 -0400

    Forge-server-setup.sh
    Updated version 1.2

--- Forge-server-setup.sh
+++ Forge-server-setup.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 #Madlib Copyright MysteryManYT-ttv 2026
 echo "Downloading Forge version 1.20.1"
+
+#wget -P ~/Downloads https://github.com/MysteryManYT-ttv/Forge-1.20.1/archive/refs/heads/main.zip"
 wget "https://github.com/MysteryManYT-ttv/Forge-1.20.1/archive/refs/heads/main.zip"
+unzip ~/main.zip
+mv ~/main.zip ~/Downloads/main
+
+#mv ~/main/Forge-1.20.1-main/ ~/Downloads
+#chmod +x  ~/main/Forge-1.20.1-main/forge-1.20.1-47.4.10-installer(1).jar"
+
+
+
+
+
+
+
+
+
 #work in progress :)
 
 
